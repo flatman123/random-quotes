076d906ea0d6bd22727fef21b61b2db15dc5d64f
jeffrey.mcintyre <redeyecoding@gmail.com>
2020-04-20 10:03:37 -0700

    react: Tested react, sucessful

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../styles/styles.scss'
+
+
+class App extends React.Component{
+    render(){
+        return (
+            <h1>MY RANDOM QUOTE APP</h1>
+        )
+    }
+};
+
+export default App;
